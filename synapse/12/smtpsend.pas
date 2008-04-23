48bb65c2c3c843a9c16fbd3ba44c4290f2bfe74a
geby <geby@7c85be65-684b-0410-a082-b2ed4fbef004>
2008-04-23 20:49:02 +0000

    made a copy
    git-svn-id: svn://svn.code.sf.net/p/synalist/code@28 7c85be65-684b-0410-a082-b2ed4fbef004

---
+++ synapse/12/smtpsend.pas
@@ -0,0 +1,219 @@
+{==============================================================================|
+| Project : Delphree - Synapse                                   | 001.003.000 |
+|==============================================================================|
+| Content: SMTP client                                                         |
+|==============================================================================|
+| The contents of this file are subject to the Mozilla Public License Ver. 1.0 |
+| (the "License"); you may not use this file except in compliance with the     |
+| License. You may obtain a copy of the License at http://www.mozilla.org/MPL/ |
+|                                                                              |
+| Software distributed under the License is distributed on an "AS IS" basis,   |
+| WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for |
+| the specific language governing rights and limitations under the License.    |
+|==============================================================================|
+| The Original Code is Synapse Delphi Library.                                 |
+|==============================================================================|
+| The Initial Developer of the Original Code is Lukas Gebauer (Czech Republic).|
+| Portions created by Lukas Gebauer are Copyright (c) 1999.                    |
+| All Rights Reserved.                                                         |
+|==============================================================================|
+| Contributor(s):                                                              |
+|==============================================================================|
+| History: see HISTORY.HTM from distribution package                           |
+|          (Found at URL: http://www.ararat.cz/synapse/)                       |
+|==============================================================================}
+
+unit SMTPsend;
+
+interface
+uses
+  Blcksock, sysutils, classes, windows, SynaUtil;
+
+const
+  CRLF=#13+#10;
+
+type
+  TSMTPSend = class
+  private
+    Sock:TTCPBlockSocket;
+    function ReadResult:integer;
+  public
+    timeout:integer;
+    SMTPHost:string;
+    ResultCode:integer;
+    ResultString:string;
+    Constructor Create;
+    Destructor Destroy; override;
+    function login:Boolean;
+    procedure logout;
+    function reset:Boolean;
+    function noop:Boolean;
+    function mailfrom(Value:string):Boolean;
+    function mailto(Value:string):Boolean;
+    function maildata(Value:Tstrings):Boolean;
+  end;
+
+function SendtoRaw (mailfrom,mailto,SMTPHost:string;maildata:TStrings):Boolean;
+function Sendto (mailfrom,mailto,subject,SMTPHost:string;maildata:TStrings):Boolean;
+
+implementation
+
+{TSMTPSend.Create}
+Constructor TSMTPSend.Create;
+begin
+  inherited Create;
+  sock:=TTCPBlockSocket.create;
+  sock.CreateSocket;
+  timeout:=300000;
+  SMTPhost:='localhost';
+end;
+
+{TSMTPSend.Destroy}
+Destructor TSMTPSend.Destroy;
+begin
+  Sock.free;
+  inherited destroy;
+end;
+
+{TSMTPSend.ReadResult}
+function TSMTPSend.ReadResult:integer;
+var
+  s:string;
+begin
+  Result:=0;
+  s:=sock.recvstring(timeout);
+  ResultString:=s;
+  if Length(s)>=3 then Result:=Strtointdef(Copy(s,1,3),0);
+  while pos('-',s)=4 do
+    begin
+      s:=sock.recvstring(timeout);
+      if sock.LastError<>0 then
+        begin
+          Result:=0;
+          break;
+        end;
+    end;
+  ResultCode:=Result;
+end;
+
+{TSMTPSend.login}
+function TSMTPSend.login:Boolean;
+begin
+  Result:=False;
+  sock.Connect(SMTPHost,'smtp');
+  if sock.lasterror<>0 then Exit;
+  if readresult<>220 then Exit;
+  Sock.SendString('HELO '+sock.LocalName+CRLF);
+  if readresult<>250 then Exit;
+  Result:=True;
+end;
+
+{TSMTPSend.logout}
+procedure TSMTPSend.logout;
+begin
+  Sock.SendString('QUIT'+CRLF);
+  readresult;
+  Sock.CloseSocket;
+end;
+
+{TSMTPSend.reset}
+function TSMTPSend.reset:Boolean;
+begin
+  Result:=false;
+  Sock.SendString('RSET'+CRLF);
+  if readresult<>250 then Exit;
+  Result:=True;
+end;
+
+{TSMTPSend.noop}
+function TSMTPSend.noop:Boolean;
+begin
+  Result:=false;
+  Sock.SendString('NOOP'+CRLF);
+  if readresult<>250 then Exit;
+  Result:=True;
+end;
+
+
+{TSMTPSend.mailfrom}
+function TSMTPSend.mailfrom(Value:string):Boolean;
+begin
+  Result:=false;
+  Sock.SendString('MAIL FROM:<'+Value+'>'+CRLF);
+  if readresult<>250 then Exit;
+  Result:=True;
+end;
+
+{TSMTPSend.mailto}
+function TSMTPSend.mailto(Value:string):Boolean;
+begin
+  Result:=false;
+  Sock.SendString('RCPT TO:<'+Value+'>'+CRLF);
+  if readresult<>250 then Exit;
+  Result:=True;
+end;
+
+{TSMTPSend.maildata}
+function TSMTPSend.maildata(Value:Tstrings):Boolean;
+var
+  n:integer;
+  s:string;
+begin
+  Result:=false;
+  Sock.SendString('DATA'+CRLF);
+  if readresult<>354 then Exit;
+  for n:=0 to Value.Count-1 do
+    begin
+      s:=value[n];
+      if Length(s)>=1 then
+        if s[1]='.' then s:='.'+s;
+      Sock.SendString(s+CRLF);
+    end;
+  Sock.SendString('.'+CRLF);
+  if readresult<>250 then Exit;
+  Result:=True;
+end;
+
+{==============================================================================}
+
+function SendtoRaw (mailfrom,mailto,SMTPHost:string;maildata:TStrings):Boolean;
+var
+  SMTP:TSMTPSend;
+begin
+  Result:=False;
+  SMTP:=TSMTPSend.Create;
+  try
+    SMTP.SMTPHost:=SMTPHost;
+    if not SMTP.login then Exit;
+    if not SMTP.mailfrom(mailfrom) then Exit;
+    if not SMTP.mailto(mailto) then Exit;
+    if not SMTP.maildata(Maildata) then Exit;
+    SMTP.logout;
+    Result:=True;
+  finally
+    SMTP.Free;
+  end;
+end;
+
+function Sendto (mailfrom,mailto,subject,SMTPHost:string;maildata:TStrings):Boolean;
+var
+  t:TStrings;
+begin
+  Result:=False;
+  t:=TStringList.Create;
+  try
+    t.assign(Maildata);
+    t.Insert(0,'');
+    t.Insert(0,'x-mailer: Synapse - Delphi TCP/IP library by Lukas Gebauer');
+    t.Insert(0,'subject: '+subject);
+    t.Insert(0,'date: '+Rfc822DateTime(now));
+    t.Insert(0,'to: '+mailto);
+    t.Insert(0,'from: '+mailfrom);
+    result:=SendToRaw(mailfrom,mailto,SMTPHost,t);
+  finally
+    t.Free;
+  end;
+end;
+
+
+end.
---
+++ synapse/12/snmptrap.pas
@@ -0,0 +1,330 @@
+{==============================================================================|
+| Project : Delphree - Synapse                                   | 002.000.001 |
+|==============================================================================|
+| Content: SNMP traps                                                          |
+|==============================================================================|
+| The contents of this file are subject to the Mozilla Public License Ver. 1.0 |
+| (the "License"); you may not use this file except in compliance with the     |
+| License. You may obtain a copy of the License at http://www.mozilla.org/MPL/ |
+|                                                                              |
+| Software distributed under the License is distributed on an "AS IS" basis,   |
+| WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for |
+| the specific language governing rights and limitations under the License.    |
+|==============================================================================|
+| The Original Code is Synapse Delphi Library.                                 |
+|==============================================================================|
+| The Initial Developer of the Original Code is Lukas Gebauer (Czech Republic).|
+| Portions created by Hernan Sanchez are Copyright (c) 2000.                   |
+| All Rights Reserved.                                                         |
+|==============================================================================|
+| Contributor(s):                                                              |
+|   Hernan Sanchez (hernan.sanchez@iname.com)                                  |
+|==============================================================================|
+| History: see HISTORY.HTM from distribution package                           |
+|          (Found at URL: http://www.ararat.cz/synapse/)                       |
+|==============================================================================}
+
+unit SNMPTrap;
+
+interface
+
+uses
+  Classes, SysUtils, BlckSock, SynaUtil, ASN1Util, SNMPsend;
+
+const
+  TRAP_PORT      = 162;
+
+  SNMP_VERSION   = 0;
+
+  PDU_GET        = $A0;
+  PDU_GETN       = $A1;
+  PDU_RESP       = $A2;
+  PDU_SET        = $A3;
+  PDU_TRAP       = $A4;
+
+type
+  TTrapPDU = class(TObject)
+    private
+    protected
+      Buffer: string;
+    public
+      TrapPort: integer;
+      Version: integer;
+      PDUType: integer;
+      Community: string;
+      Enterprise: string;
+      TrapHost: string;
+      GenTrap: integer;
+      SpecTrap: integer;
+      TimeTicks: integer;
+      SNMPMibList: TList;
+      constructor Create;
+      destructor Destroy; override;
+      procedure Clear;
+      procedure MIBAdd(MIB, Value: string; ValueType:integer);
+      procedure MIBDelete(Index: integer);
+      function MIBGet(MIB: string): string;
+      function EncodeTrap: integer;
+      function DecodeTrap: integer;
+  end;
+
+  TTrapSNMP = class(TObject)
+  private
+    sock: TUDPBlockSocket;
+  public
+    Trap: TTrapPDU;
+    SNMPHost: string;
+    Timeout: integer;
+    constructor Create;
+    destructor Destroy; override;
+    function Send: integer;
+    function Recv: integer;
+  end;
+
+function SendTrap(Dest, Source, Enterprise, Community: string;
+  Generic, Specific, Seconds: integer; MIBName, MIBValue: string; MIBtype:integer): integer;
+function RecvTrap(var Dest, Source, Enterprise, Community: string;
+  var Generic, Specific, Seconds: integer; var MIBName, MIBValue: TStringList): integer;
+
+implementation
+
+constructor TTrapPDU.Create;
+begin
+  inherited Create;
+  SNMPMibList := TList.create;
+  TrapPort := TRAP_PORT;
+  Version := SNMP_VERSION;
+  PDUType := PDU_TRAP;
+  Community := 'public';
+end;
+
+destructor TTrapPDU.Destroy;
+begin
+  SNMPMibList.free;
+  inherited Destroy;
+end;
+
+procedure TTrapPDU.Clear;
+var
+  i:integer;
+begin
+  for i := 0 to SNMPMibList.count - 1 do
+    TSNMPMib(SNMPMibList[i]).Free;
+  SNMPMibList.Clear;
+  TrapPort := TRAP_PORT;
+  Version := SNMP_VERSION;
+  PDUType := PDU_TRAP;
+  Community := 'public';
+end;
+
+procedure TTrapPDU.MIBAdd(MIB, Value: string; ValueType:integer);
+var
+  SNMPMib: TSNMPMib;
+begin
+  SNMPMib := TSNMPMib.Create;
+  SNMPMib.OID := MIB;
+  SNMPMib.Value := Value;
+  SNMPMib.ValueType := ValueType;
+  SNMPMibList.Add(SNMPMib);
+end;
+
+procedure TTrapPDU.MIBDelete(Index: integer);
+begin
+  if (Index >= 0) and (Index < SNMPMibList.count) then
+    begin
+      TSNMPMib(SNMPMibList[Index]).Free;
+      SNMPMibList.Delete(Index);
+    end;
+end;
+
+function TTrapPDU.MIBGet(MIB: string): string;
+var
+  i: integer;
+begin
+  Result := '';
+  for i := 0 to SNMPMibList.count - 1 do
+    begin
+      if ((TSNMPMib(SNMPMibList[i])).OID = MIB) then
+      begin
+        Result := (TSNMPMib(SNMPMibList[i])).Value;
+        break;
+      end;
+    end;
+end;
+
+function TTrapPDU.EncodeTrap: integer;
+var
+  s: string;
+  n: integer;
+  SNMPMib: TSNMPMib;
+begin
+  Buffer := '';
+  for n:=0 to SNMPMibList.Count-1 do
+    begin
+      SNMPMib := SNMPMibList[n];
+      case (SNMPMib.ValueType) of
+        ASN1_INT, ASN1_COUNTER, ASN1_GAUGE, ASN1_TIMETICKS:
+          begin
+            s := ASNObject(MibToID(SNMPMib.OID),ASN1_OBJID)
+              +ASNObject(ASNEncInt(strToIntDef(SNMPMib.Value,0)),SNMPMib.ValueType);
+          end;
+        ASN1_OBJID:
+          begin
+            s := ASNObject(MibToID(SNMPMib.OID),ASN1_OBJID) + ASNObject(MibToID(SNMPMib.Value),SNMPMib.ValueType);
+          end;
+        ASN1_IPADDR:
+          begin
+            s := ASNObject(MibToID(SNMPMib.OID),ASN1_OBJID) + ASNObject(IPToID(SNMPMib.Value),SNMPMib.ValueType);
+          end;
+        ASN1_NULL:
+          begin
+            s := ASNObject(MibToID(SNMPMib.OID),ASN1_OBJID) + ASNObject('',ASN1_NULL);
+          end;
+        else
+          s := ASNObject(MibToID(SNMPMib.OID),ASN1_OBJID) + ASNObject(SNMPMib.Value,SNMPMib.ValueType);
+      end;
+      Buffer := Buffer + ASNObject(s, ASN1_SEQ);
+    end;
+  Buffer := ASNObject(Buffer, ASN1_SEQ);
+  Buffer := ASNObject(ASNEncInt(GenTrap), ASN1_INT)
+    + ASNObject(ASNEncInt(SpecTrap), ASN1_INT)
+    + ASNObject(ASNEncInt(TimeTicks), ASN1_TIMETICKS)
+    + Buffer;
+  Buffer := ASNObject(MibToID(Enterprise), ASN1_OBJID)
+    + ASNObject(IPToID(TrapHost), ASN1_IPADDR)
+    + Buffer;
+  Buffer := ASNObject(Char(Version), ASN1_INT)
+    + ASNObject(Community, ASN1_OCTSTR)
+    + ASNObject(Buffer, Self.PDUType);
+  Buffer := ASNObject(Buffer, ASN1_SEQ);
+  Result := 1;
+end;
+
+function TTrapPDU.DecodeTrap: integer;
+var
+  Pos, EndPos: integer;
+  Sm, Sv: string;
+  Svt:integer;
+begin
+  clear;
+  Pos := 2;
+  EndPos := ASNDecLen(Pos, Buffer);
+  Version := StrToIntDef(ASNItem(Pos, Buffer,svt), 0);
+  Community := ASNItem(Pos, Buffer,svt);
+  PDUType := StrToIntDef(ASNItem(Pos, Buffer,svt), PDU_TRAP);
+  Enterprise := ASNItem(Pos, Buffer,svt);
+  TrapHost := ASNItem(Pos, Buffer,svt);
+  GenTrap := StrToIntDef(ASNItem(Pos, Buffer,svt), 0);
+  Spectrap := StrToIntDef(ASNItem(Pos, Buffer,svt), 0);
+  TimeTicks := StrToIntDef(ASNItem(Pos, Buffer,svt), 0);
+  ASNItem(Pos, Buffer,svt);
+  while (Pos < EndPos) do
+    begin
+      ASNItem(Pos, Buffer,svt);
+      Sm := ASNItem(Pos, Buffer,svt);
+      Sv := ASNItem(Pos, Buffer,svt);
+      MIBAdd(Sm, Sv, svt);
+    end;
+  Result := 1;
+end;
+
+constructor TTrapSNMP.Create;
+begin
+  inherited Create;
+  Sock := TUDPBlockSocket.Create;
+  Trap := TTrapPDU.Create;
+  Timeout := 5000;
+  SNMPHost := '127.0.0.1';
+  Sock.CreateSocket;
+end;
+
+destructor TTrapSNMP.Destroy;
+begin
+  Trap.Free;
+  Sock.Free;
+  inherited Destroy;
+end;
+
+function TTrapSNMP.Send: integer;
+begin
+  Trap.EncodeTrap;
+  Sock.Connect(SNMPHost, IntToStr(Trap.TrapPort));
+  Sock.SendBuffer(PChar(Trap.Buffer), Length(Trap.Buffer));
+  Result := 1;
+end;
+
+function TTrapSNMP.Recv: integer;
+var
+  x: integer;
+begin
+  Result := 0;
+  Sock.Bind('0.0.0.0', IntToStr(Trap.TrapPort));
+  if Sock.CanRead(Timeout) then
+    begin
+      x := Sock.WaitingData;
+      if (x > 0) then
+        begin
+          SetLength(Trap.Buffer, x);
+          Sock.RecvBuffer(PChar(Trap.Buffer), x);
+          Trap.DecodeTrap;
+          Result := 1;
+        end;
+    end;
+end;
+
+function SendTrap(Dest, Source, Enterprise, Community: string;
+  Generic, Specific, Seconds: integer; MIBName, MIBValue: string; MIBtype:integer): integer;
+var
+  SNMP: TTrapSNMP;
+begin
+  SNMP := TTrapSNMP.Create;
+  try
+    SNMP.SNMPHost := Dest;
+    SNMP.Trap.TrapHost := Source;
+    SNMP.Trap.Enterprise := Enterprise;
+    SNMP.Trap.Community := Community;
+    SNMP.Trap.GenTrap := Generic;
+    SNMP.Trap.SpecTrap := Specific;
+    SNMP.Trap.TimeTicks := Seconds;
+    SNMP.Trap.MIBAdd(MIBName,MIBValue,MIBType);
+    Result := SNMP.Send;
+  finally
+    SNMP.Free;
+  end;
+end;
+
+function RecvTrap(var Dest, Source, Enterprise, Community: string;
+  var Generic, Specific, Seconds: integer; var MIBName, MIBValue: TStringList):
+integer;
+var
+  SNMP: TTrapSNMP;
+  i: integer;
+begin
+  SNMP := TTrapSNMP.Create;
+  try
+    SNMP.SNMPHost := Dest;
+    Result := SNMP.Recv;
+    if (Result <> 0) then
+    begin
+      Dest := SNMP.SNMPHost;
+      Source := SNMP.Trap.TrapHost;
+      Enterprise := SNMP.Trap.Enterprise;
+      Community := SNMP.Trap.Community;
+      Generic := SNMP.Trap.GenTrap;
+      Specific := SNMP.Trap.SpecTrap;
+      Seconds := SNMP.Trap.TimeTicks;
+      MIBName.Clear;
+      MIBValue.Clear;
+      for i:=0 to (SNMP.Trap.SNMPMibList.count - 1) do
+        begin
+          MIBName.Add(TSNMPMib(SNMP.Trap.SNMPMibList[i]).OID);
+          MIBValue.Add(TSNMPMib(SNMP.Trap.SNMPMibList[i]).Value);
+        end;
+    end;
+  finally
+    SNMP.Free;
+  end;
+end;
+
+end.
+
